615af50e28a221b3e642461214cba4b190313e1d
Kerim Mustafa Demir <kerimmstfdemir@gmail.com>
2023-07-01 13:07:35 +0300

    MessageBox operation done

---
+++ Tool_Usages/Tool_Usages/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Tool_Usages
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Corbel", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(153, 113);
+            button1.Name = "button1";
+            button1.Size = new Size(138, 36);
+            button1.TabIndex = 0;
+            button1.Text = "Click";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.SteelBlue;
+            ClientSize = new Size(441, 297);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "First Code";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+    }
+}
