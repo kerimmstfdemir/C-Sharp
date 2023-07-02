16748bb6982c789ab1678e488ec88028ad91feb2
Kerim Mustafa Demir <kerimmstfdemir@gmail.com>
2023-07-02 22:25:39 +0300

    label and textbox usage

--- Tool_Usages/Tool_Usages/Form1.Designer.cs
+++ Tool_Usages/Tool_Usages/Form1.Designer.cs
@@ -29,34 +29,208 @@
         private void InitializeComponent()
         {
             button1 = new Button();
+            label1 = new Label();
+            button2 = new Button();
+            label2 = new Label();
+            button3 = new Button();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            textBox1 = new TextBox();
+            button4 = new Button();
+            label9 = new Label();
             SuspendLayout();
             // 
             // button1
             // 
             button1.Font = new Font("Corbel", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            button1.Location = new Point(153, 113);
+            button1.Location = new Point(43, 121);
             button1.Name = "button1";
-            button1.Size = new Size(138, 36);
+            button1.Size = new Size(120, 36);
             button1.TabIndex = 0;
             button1.Text = "Click";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Corbel", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(43, 31);
+            label1.Name = "label1";
+            label1.Size = new Size(66, 23);
+            label1.TabIndex = 1;
+            label1.Text = "Name :";
+            label1.Click += label1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Corbel", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(43, 73);
+            button2.Name = "button2";
+            button2.Size = new Size(120, 34);
+            button2.TabIndex = 2;
+            button2.Text = "Print";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(106, 32);
+            label2.Name = "label2";
+            label2.Size = new Size(57, 21);
+            label2.TabIndex = 3;
+            label2.Text = "label2";
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.DarkGray;
+            button3.Location = new Point(362, 121);
+            button3.Name = "button3";
+            button3.Size = new Size(126, 36);
+            button3.TabIndex = 4;
+            button3.Text = "Get Information";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Georgia", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(343, 37);
+            label3.Name = "label3";
+            label3.Size = new Size(111, 18);
+            label3.TabIndex = 5;
+            label3.Text = "Your Name :";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Georgia", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(316, 62);
+            label4.Name = "label4";
+            label4.Size = new Size(138, 18);
+            label4.TabIndex = 6;
+            label4.Text = "Your Surname :";
+            label4.Click += label4_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Georgia", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(362, 89);
+            label5.Name = "label5";
+            label5.Size = new Size(92, 18);
+            label5.TabIndex = 7;
+            label5.Text = "Your Job :";
+            label5.Click += label5_Click;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Georgia", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.Location = new Point(460, 37);
+            label6.Name = "label6";
+            label6.Size = new Size(50, 18);
+            label6.TabIndex = 8;
+            label6.Text = "label6";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Georgia", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.Location = new Point(460, 62);
+            label7.Name = "label7";
+            label7.Size = new Size(49, 18);
+            label7.TabIndex = 9;
+            label7.Text = "label7";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Georgia", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label8.Location = new Point(460, 89);
+            label8.Name = "label8";
+            label8.Size = new Size(50, 18);
+            label8.TabIndex = 10;
+            label8.Text = "label8";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Georgia", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(43, 194);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(154, 26);
+            textBox1.TabIndex = 11;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(43, 226);
+            button4.Name = "button4";
+            button4.Size = new Size(154, 33);
+            button4.TabIndex = 12;
+            button4.Text = "Transfer";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label9.Location = new Point(316, 194);
+            label9.Name = "label9";
+            label9.Size = new Size(51, 20);
+            label9.TabIndex = 13;
+            label9.Text = "label9";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.SteelBlue;
-            ClientSize = new Size(441, 297);
+            BackColor = Color.MediumAquamarine;
+            ClientSize = new Size(605, 297);
+            Controls.Add(label9);
+            Controls.Add(button4);
+            Controls.Add(textBox1);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(button3);
+            Controls.Add(label2);
+            Controls.Add(button2);
+            Controls.Add(label1);
             Controls.Add(button1);
             Name = "Form1";
-            Text = "First Code";
+            Text = "Tool Usages";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button button1;
+        private Label label1;
+        private Button button2;
+        private Label label2;
+        private Button button3;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private TextBox textBox1;
+        private Button button4;
+        private Label label9;
     }
 }
--- Tool_Usages/Tool_Usages/Form1.cs
+++ Tool_Usages/Tool_Usages/Form1.cs
@@ -16,5 +16,43 @@ namespace Tool_Usages
         {
             MessageBox.Show("Hello C#");
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Kerim";
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label6.Text = "Kerim Mustafa";
+            label7.Text = "Demir";
+            label8.Text = "Software Developer";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // textBox1.Text = "Software Engineer";
+            label9.Text = textBox1.Text;
+        }
     }
 }
