52635f22ba9435e79044d61252bf2363724f6ead
Kerim Mustafa Demir <kerimmstfdemir@gmail.com>
2023-07-25 23:57:37 +0300

    added flight infos for plane ticeket reservation project

--- Projects/b. Plane Ticket Reservation System/b. Plane Ticket Reservation System/Form1.Designer.cs
+++ Projects/b. Plane Ticket Reservation System/b. Plane Ticket Reservation System/Form1.Designer.cs
@@ -28,12 +28,142 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            label1 = new Label();
+            label2 = new Label();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            label3 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            label4 = new Label();
+            maskedTextBox1 = new MaskedTextBox();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(maskedTextBox1);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(dateTimePicker1);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(comboBox2);
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(30, 129);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(405, 284);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Flight Informations";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Location = new Point(481, 129);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(405, 284);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Passenger Informations";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(25, 41);
+            label1.Name = "label1";
+            label1.Size = new Size(56, 18);
+            label1.TabIndex = 0;
+            label1.Text = "From :";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(45, 76);
+            label2.Name = "label2";
+            label2.Size = new Size(36, 18);
+            label2.TabIndex = 1;
+            label2.Text = "To :";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "İstanbul", "Ankara", "İzmir", "Erzurum", "Konya", "Mardin", "Samsun" });
+            comboBox1.Location = new Point(90, 38);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(154, 26);
+            comboBox1.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Items.AddRange(new object[] { "İstanbul", "Ankara", "İzmir", "Erzurum", "Konya", "Mardin", "Samsun" });
+            comboBox2.Location = new Point(90, 73);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(154, 26);
+            comboBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(31, 111);
+            label3.Name = "label3";
+            label3.Size = new Size(50, 18);
+            label3.TabIndex = 4;
+            label3.Text = "Date :";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(90, 108);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(154, 26);
+            dateTimePicker1.TabIndex = 5;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(27, 146);
+            label4.Name = "label4";
+            label4.Size = new Size(54, 18);
+            label4.TabIndex = 6;
+            label4.Text = "Time :";
+            // 
+            // maskedTextBox1
+            // 
+            maskedTextBox1.Location = new Point(90, 143);
+            maskedTextBox1.Mask = "00:00";
+            maskedTextBox1.Name = "maskedTextBox1";
+            maskedTextBox1.Size = new Size(154, 26);
+            maskedTextBox1.TabIndex = 7;
+            maskedTextBox1.ValidatingType = typeof(DateTime);
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 18F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(911, 477);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Font = new Font("Georgia", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            Margin = new Padding(4, 4, 4, 4);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private DateTimePicker dateTimePicker1;
+        private Label label3;
+        private ComboBox comboBox2;
+        private ComboBox comboBox1;
+        private Label label2;
+        private Label label1;
+        private MaskedTextBox maskedTextBox1;
+        private Label label4;
     }
 }
