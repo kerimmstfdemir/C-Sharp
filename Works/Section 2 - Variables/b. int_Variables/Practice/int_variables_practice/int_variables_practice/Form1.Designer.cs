b84001a54ce628f78314eb356cf013e633735c8c
Kerim Mustafa Demir <kerimmstfdemir@gmail.com>
2023-08-08 00:06:27 +0300

    added average calculation work

--- Works/Section 2 - Variables/b. int_Variables/Practice/int_variables_practice/int_variables_practice/Form1.Designer.cs
+++ Works/Section 2 - Variables/b. int_Variables/Practice/int_variables_practice/int_variables_practice/Form1.Designer.cs
@@ -33,23 +33,29 @@
             label3 = new Label();
             button1 = new Button();
             listBox1 = new ListBox();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            button2 = new Button();
+            listBox2 = new ListBox();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(32, 18);
+            label1.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point);
+            label1.Location = new Point(19, 16);
             label1.Name = "label1";
-            label1.Size = new Size(78, 20);
+            label1.Size = new Size(86, 20);
             label1.TabIndex = 0;
-            label1.Text = "Rectangle";
+            label1.Text = "Rectangle :";
             // 
             // label2
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
-            label2.Location = new Point(33, 48);
+            label2.Location = new Point(20, 46);
             label2.Name = "label2";
             label2.Size = new Size(46, 17);
             label2.TabIndex = 1;
@@ -59,7 +65,7 @@
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
-            label3.Location = new Point(33, 74);
+            label3.Location = new Point(20, 72);
             label3.Name = "label3";
             label3.Size = new Size(47, 17);
             label3.TabIndex = 2;
@@ -69,7 +75,7 @@
             // button1
             // 
             button1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            button1.Location = new Point(32, 103);
+            button1.Location = new Point(19, 101);
             button1.Name = "button1";
             button1.Size = new Size(78, 23);
             button1.TabIndex = 3;
@@ -81,17 +87,83 @@
             // 
             listBox1.FormattingEnabled = true;
             listBox1.ItemHeight = 15;
-            listBox1.Location = new Point(140, 47);
+            listBox1.Location = new Point(127, 45);
             listBox1.Name = "listBox1";
             listBox1.Size = new Size(258, 79);
             listBox1.TabIndex = 4;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point);
+            label4.Location = new Point(20, 173);
+            label4.Name = "label4";
+            label4.Size = new Size(156, 20);
+            label4.TabIndex = 5;
+            label4.Text = "Average Calculation :";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(21, 232);
+            label5.Name = "label5";
+            label5.Size = new Size(74, 17);
+            label5.TabIndex = 7;
+            label5.Text = "Note 2 : 82";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(23, 205);
+            label6.Name = "label6";
+            label6.Size = new Size(72, 17);
+            label6.TabIndex = 6;
+            label6.Text = "Note 1 : 90";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.Location = new Point(21, 259);
+            label7.Name = "label7";
+            label7.Size = new Size(74, 17);
+            label7.TabIndex = 7;
+            label7.Text = "Note 3 : 75";
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(21, 289);
+            button2.Name = "button2";
+            button2.Size = new Size(77, 23);
+            button2.TabIndex = 8;
+            button2.Text = "Calculate";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.ItemHeight = 15;
+            listBox2.Location = new Point(127, 202);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(258, 109);
+            listBox2.TabIndex = 4;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Lavender;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(410, 335);
+            Controls.Add(button2);
+            Controls.Add(label7);
+            Controls.Add(label5);
+            Controls.Add(label6);
+            Controls.Add(label4);
+            Controls.Add(listBox2);
             Controls.Add(listBox1);
             Controls.Add(button1);
             Controls.Add(label3);
@@ -110,5 +182,11 @@
         private Label label3;
         private Button button1;
         private ListBox listBox1;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Button button2;
+        private ListBox listBox2;
     }
 }
--- Works/Section 2 - Variables/b. int_Variables/Practice/int_variables_practice/int_variables_practice/Form1.cs
+++ Works/Section 2 - Variables/b. int_Variables/Practice/int_variables_practice/int_variables_practice/Form1.cs
@@ -14,14 +14,31 @@ namespace int_variables_practice
 
         private void button1_Click(object sender, EventArgs e)
         {
+            listBox1.Items.Clear();
+
             int a, b, area, perimeter;
-            a = 10; 
+            a = 10;
             b = 20;
 
             area = a * b;
             perimeter = 2 * (a + b);
 
             listBox1.Items.Add("Area : " + area + " - Perimeter : " + perimeter);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+
+            int note1, note2, note3, mean;
+            note1 = 90;
+            note2 = 82;
+            note3 = 75;
+
+            mean = (note1 + note2 + note3) / 3;
+
+            listBox2.Items.Add("Average Note : " + mean);
+
         }
     }
 }
